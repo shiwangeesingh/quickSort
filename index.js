8f52ebb9305120e9a3fd545859e81bfa7e6c9dc4
Shiwangee Singh <shiwangee.singh@fluper.in>
2020-01-30 19:46:00 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,3 @@
+let sort = require('./quicksort')
+const array = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8];
+sort.quicksortfunc(array)
---
+++ quicksort.js
@@ -0,0 +1,15 @@
+exports.quicksortfunc = function quickSort(arr) {
+    if (arr.length < 2) {
+        return arr;
+    }
+    let left = []; let right = []
+    const [pivot, ...rest] = arr
+   rest.forEach(element => {
+    element >= pivot ? right.push(element) : left.push(element) 
+   })
+   console.log(quickSort(left).concat(pivot).concat(quickSort(right)))
+    return quickSort(left).concat(pivot).concat(quickSort(right))
+}
+
+const array = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8];
+
